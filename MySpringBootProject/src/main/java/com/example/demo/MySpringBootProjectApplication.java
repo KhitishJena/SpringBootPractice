390f70379986c75f00e7a48ada203cbcbe5193ae
KhitishJena <satishkumarjena307@gmail.com>
2025-03-16 22:44:40 +0530

    first sample project

---
+++ MySpringBootProject/src/main/java/com/example/demo/MySpringBootProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootProjectApplication.class, args);
+		
+		System.out.println("~~~~~~~~~~~~~~~~MY first Springboot project**************");
+	}
+
+}
