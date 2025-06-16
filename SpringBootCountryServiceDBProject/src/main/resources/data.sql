4a543f2d64d4abc09b58d60e0ecb5a2603ab9d22
KhitishJena <satishkumarjena307@gmail.com>
2025-06-16 21:42:55 +0530

    added properties for H2Database

---
+++ SpringBootCountryServiceDBProject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+CREATE TABLE samplecountry (id int NOT NULL, countryname varchar(30) DEFAULT NULL, countrycapital varchar(30) DEFAULT NULL, PRIMARY KEY (id));
+  INSERT INTO samplecountry values (1, 'India', 'Delhi');
+  INSERT INTO samplecountry values (2, 'USA', 'NewYork');
